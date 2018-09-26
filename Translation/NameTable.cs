5858cc1c2e810ee3fd4941a8b53b0ea5f8340a9f
hussaini said adam <hussainisaidadam@gmail.com>
2018-09-26 23:02:33 +0300

    Added Identifier

--- Translation/NameTable.cs
+++ Translation/NameTable.cs
@@ -4,8 +4,43 @@ using System.Text;
 
 namespace Translation
 {
-    class NameTable
+    public static class NameTable
     {
+        public enum TCat
+        {
+            Const, Var, Type
+        };
+        public enum TType
+        {
+            None, Int, Bool
+        };
+         public  struct Identifier
+        {
+            public string name;
+            public TCat cat;
+            public TType type; 
+        }
 
+        public static LinkedList<Identifier> Identifiers = new LinkedList<Identifier>();
+
+        public static Identifier  Addidentifier(string name,TCat cat)
+        {
+            Identifier identifier = new Identifier
+            {
+                name = name,
+                cat = cat
+            };
+            Identifiers.AddLast(identifier);
+            return identifier;
+        }
+        public static Identifier SearchByName(string name)
+        {
+            LinkedListNode<Identifier> node = Identifiers.First;
+            while (node!=null & node.Value.name!=name)
+            {
+                node = node.Next;
+            }
+            return node == null ? new Identifier() : node.Value;
+        }
     }
 }
