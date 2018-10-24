7fb326f452637b4367cb84f10158d4c5376366bd
hooseinsaid <hussainisaidadam@hmail.com>
2018-10-24 20:48:03 +0300

    Test

---
+++ Test/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Translation
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LexicalTest();
+        }
+        static void LexicalTest()
+        {
+            Reader.Initialize(@"C:\Users\hussa\OneDrive\Apps\Desktop\newtonhor.txt");
+            test.Initialize();
+            while( test.currentLexem!=test.Lexems.EOF)
+            {
+                Console.WriteLine("string {0} {1} -> {2}", Reader.NLines, LexicalAnalyzer.currentName,LexicalAnalyzer.currentLexem);
+                LexicalAnalyzer.ParseNextLexem();
+            }
+
+        }
+    }
+}
