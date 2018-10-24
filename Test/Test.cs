4b3d5b03cff07e2f84ccde007f9e774d7be3b947
hooseinsaid <hussainisaidadam@hmail.com>
2018-10-24 23:31:23 +0300

    Test

--- Test/Test.cs
+++ Test/Test.cs
@@ -5,33 +5,42 @@ namespace Translation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           // Console.WriteLine("Hello World!");
             LexicalTest();
         }
         static void LexicalTest()
         {
+
             Reader.Initialize(@"C:\Users\hussa\OneDrive\Apps\Desktop\newtonhor.txt");
-            //test.Initialize();
-            //while( test.currentLexem!=test.Lexems.EOF)
-            //{
-            //   // Console.WriteLine("string {0} {1} -> {2}", Reader.NLines, LexicalAnalyzer.currentName,LexicalAnalyzer.currentLexem);
-            //    LexicalAnalyzer.ParseNextLexem();
-            //}
-
-
             LexicalAnalyzer.Initialize();
-            Reader.Initialize(@"C:\Users\hussa\OneDrive\Apps\Desktop\newtonhor.txt");
             string lexlemename = "";
             LexicalAnalyzer.Lexems lex;
-            while (Reader.CurSymbol != 0)
+            while (Reader.CurSymbol!=0)
             {
                 LexicalAnalyzer.ParseNextLexem();
                 lexlemename = LexicalAnalyzer.currentName;
                 lex = LexicalAnalyzer.currentLexem;
                 Console.WriteLine("string {0} {1} -> {2}", Reader.NLines, lexlemename, lex.ToString());
-                LexicalAnalyzer.ParseNextLexem();
 
             }
+            Console.ReadLine();
+
+
+
+
+            //LexicalAnalyzer.Initialize();
+            //Reader.Initialize(@"C:\Users\hussa\OneDrive\Apps\Desktop\newtonhor.txt");
+            //string lexlemename = "";
+            //LexicalAnalyzer.Lexems lex;
+            //while (Reader.CurSymbol != 0)
+            //{
+            //    LexicalAnalyzer.ParseNextLexem();
+            //    lexlemename = LexicalAnalyzer.currentName;
+            //    lex = LexicalAnalyzer.currentLexem;
+            //    Console.WriteLine("string {0} {1} -> {2}", Reader.NLines, lexlemename, lex.ToString());
+            //    LexicalAnalyzer.ParseNextLexem();
+
+            //}
 
         }
     }
