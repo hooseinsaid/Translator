1acc0a008d4d15d014f63773292b51bfbef51f20
hooseinsaid <hussainisaidadam@hmail.com>
2018-10-25 12:22:35 +0300

    SyntaxAnalyzer Step 5 Completed And Successful

--- Translation/SyntaxAnalyzer.cs
+++ Translation/SyntaxAnalyzer.cs
@@ -5,14 +5,30 @@ using static Translation.LexicalAnalyzer;
 
 namespace Translation
 {
-   public static class SyntaxAnalyzer
+    public struct Error
     {
-         static List<string> ErrorCatcher = new List<string>(); 
+        public int Line;
+        public int Position;
+        public string ErrorMessage;
+    }
+    public static class SyntaxAnalyzer
+    {
+        public static List<Error> Errors { get; } = new List<Error>();
+        private static void  Error(string ErrorMessage)
+        {
+            Errors.Add(new Error 
+            {
+                Line = Reader.NLines,
+                Position = Reader.PosSimLine,
+                ErrorMessage = ErrorMessage
+            });
+        }
         public static void CheckLexems(Lexems ExpectedLexem)
         {
+            ParseNextLexem();
             if (currentLexem != ExpectedLexem)
             {
-                Error();
+               
             }
             else
                 ParseNextLexem();
@@ -22,34 +38,34 @@ namespace Translation
             CheckLexems(Lexems.DataType);
             if (currentLexem!=Lexems.Identificator)
             {
-                Error();
+                Error(" DataType Identificator  error message");
             }
             else
             {
-                NameTable.Addidentifier(currentName, NameTable.TCat.Var);
+                NameTable.Addidentifier(currentName, TCat.Var);
                 ParseNextLexem();
             }
             while (currentLexem==Lexems.Comma)
             {
+                ParseNextLexem();
                 if (currentLexem != Lexems.Identificator)
-                    Error();
+                {
+                    Error("Identificator  error message");
+                }
                 else
                 {
-                    NameTable.Addidentifier(currentName, NameTable.TCat.Var);
+                    NameTable.Addidentifier(currentName, TCat.Var);
+                    ParseNextLexem();
                 }
             }
+            
         }
 
         public static void Compile()
         {
             Initialize();
             ParseVariablesDeclaration();
-            CheckLexems(Lexems.Seperator);
-        }
-
-        private static void Error()
-        {
-            throw new NotImplementedException();
+            CheckLexems(Lexems.EndOfOPeration);
         }
     }
 }
