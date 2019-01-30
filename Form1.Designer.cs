1116ee88675664079f15fae9a72244562f2ab3ef
hussaini said adam <hussainisaidadam@gmail.com>
2019-01-30 21:24:40 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace translationsASM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.compileToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.lexicalanalyzerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(371, 273);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(402, 33);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(386, 273);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(12, 312);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(776, 126);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.compileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openFileToolStripMenuItem.Text = "openFile";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // compileToolStripMenuItem
+            // 
+            this.compileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.compileToolStripMenuItem1,
+            this.lexicalanalyzerToolStripMenuItem});
+            this.compileToolStripMenuItem.Name = "compileToolStripMenuItem";
+            this.compileToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.compileToolStripMenuItem.Text = "compile";
+            // 
+            // compileToolStripMenuItem1
+            // 
+            this.compileToolStripMenuItem1.Name = "compileToolStripMenuItem1";
+            this.compileToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.compileToolStripMenuItem1.Text = "compile";
+            this.compileToolStripMenuItem1.Click += new System.EventHandler(this.compileToolStripMenuItem1_Click);
+            // 
+            // lexicalanalyzerToolStripMenuItem
+            // 
+            this.lexicalanalyzerToolStripMenuItem.Name = "lexicalanalyzerToolStripMenuItem";
+            this.lexicalanalyzerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lexicalanalyzerToolStripMenuItem.Text = "lexicalanalyzer";
+            this.lexicalanalyzerToolStripMenuItem.Click += new System.EventHandler(this.lexicalanalyzerToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem compileToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem lexicalanalyzerToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using System.Windows.Forms;
+
+namespace translationsASM
+{
+    public partial class Form1 : Form
+    {
+        private static string codeFile;
+        private static string code;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog nf = new OpenFileDialog();
+            richTextBox1.Text = "";
+            nf.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            nf.RestoreDirectory = true;
+            if (nf.ShowDialog() == DialogResult.OK)
+            {
+                codeFile = nf.FileName.ToString();
+                code = File.ReadAllText(codeFile);
+                richTextBox1.Text += code;
+            }
+            Reader.Initialize(codeFile);
+        }
+        public static int k = 0;
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            k++;
+            string outputPath = "C:\\translator\\code" + k.ToString() + ".asm";
+            SaveCode(outputPath);
+        }
+        private static void SaveCode(string nameOutputFile)
+        {
+            string newFileName = codeFile;
+            string codeOutput = "";
+            if (CodeGenerator.code != null)
+            {
+                foreach (var item in CodeGenerator.code)
+                    codeOutput += item + "\r\n";
+
+                System.IO.File.WriteAllText(nameOutputFile, codeOutput);
+                MessageBox.Show("Код сохранен в: " + nameOutputFile + ".");
+            }
+            else
+                MessageBox.Show("Код пустой.");
+        }
+
+        private void lexicalanalyzerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox3.Text = "";
+            richTextBox2.Text = "";
+            try
+            {
+                richTextBox2.Text += "Лексический анализ:" + "\r\n" + "\r\n";
+
+                Reader.Initialize(codeFile);
+                LexicalAnalyzer.Initialize();
+
+                while (LexicalAnalyzer.currentLexem != LexicalAnalyzer.Lexems.EOF)
+                {
+                    richTextBox2.Text += LexicalAnalyzer.currentLexem + " ";
+
+                    if (LexicalAnalyzer.currentLexem == LexicalAnalyzer.Lexems.Delimiter)
+                        richTextBox2.Text += "\r\n";
+
+                    if (LexicalAnalyzer.currentLexem == LexicalAnalyzer.Lexems.Identificator
+                        && NameTable.FindByName(LexicalAnalyzer.currentName).Equals(new NameTable.Identrificator()))
+                        NameTable.AddIdentrificator(LexicalAnalyzer.currentName, NameTable.tCat.Var);
+
+
+                    LexicalAnalyzer.AnalyseNextLexem();
+                }
+
+
+                LinkedListNode<NameTable.Identrificator> identFirst = NameTable.GetListIdentifier.List.First;
+
+                richTextBox2.Text += "\r\nИдентификаторы: ";
+
+                while (identFirst != null)
+                {
+                    richTextBox2.Text += identFirst.Value.name + " ";
+                    identFirst = identFirst.Next;
+                }
+
+                Reader.Close();
+            }
+            catch (Exception exp)
+            {
+                richTextBox3.Text += exp.Message;
+            }
+        }
+
+        private void compileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox2.Text += "\r\n" + "\r\n" + "Синтаксический анализ:" + "\r\n" + "\r\n";
+
+                Reader.Initialize(codeFile);
+                SyntaxAnalyzer.Compile();
+
+                if (SyntaxAnalyzer.errorMessages != null)
+                {
+                    for (int i = 0; i < SyntaxAnalyzer.errorMessages.Count; i++)
+                        richTextBox3.Text += SyntaxAnalyzer.errorMessages[i] + "\r\n";
+                }
+
+                if (CodeGenerator.code != null)
+                {
+                    for (int i = 0; i < CodeGenerator.code.Length; i++)
+                        if (CodeGenerator.code[i] != null)
+                            richTextBox2.Text += CodeGenerator.code[i] + "\r\n";
+                }
+
+                Reader.Close();
+            }
+            catch (Exception exc)
+            {
+                richTextBox3.Text += exc.Message;
+            }
+        }
+    }
+    
+}
